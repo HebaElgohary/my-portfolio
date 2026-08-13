a81b6f96b03697e5cee05a9b8eff1c8f417ea502
HebaElgohary <hebasamir535@gmail.com>
2026-08-13 16:11:56 +0300

    feat: add deployed projects on vercel

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,41 +4,41 @@ export const projects = [
     description:
       "A modern survey platform for creating, managing, and analyzing surveys.",
     tech: ["React", "Tailwind", "shadcn/ui", "Framer Motion"],
-    link: "https://surveyland.netlify.app",
+    link: "https://survey-land-mocha.vercel.app/",
   },
   {
     title: "SkillHatch",
     description:
       "A multi-language e-learning platform connecting students and instructors.",
     tech: ["React", "TypeScript", "Zustand", "i18next"],
-    link: "https://fluffy-tapioca-8f3c4a.netlify.app/",
+    link: "https://education-app-smoky.vercel.app/",
   },
   {
     title: "Medifind",
     description:
       "A secure medicine donation platform with healthcare oversight.",
     tech: ["React", "Redux", "Bootstrap"],
-    link: "https://medifindui.netlify.app/",
+    link: "https://medifind-front.vercel.app/",
   },
     {
     title: "Products e-commerce",
     description:
       "An e-commerce platform for selling products online.",
     tech: ["HTML", "CSS", "JavaScript"],
-    link: "https://productsecommerc.netlify.app/page1.html",
+    link: "https://e-commerce-eta-mocha-14.vercel.app/",
   },
     {
     title: "Jessica Portfolio – Personal Website",
     description:
       "A clean and modern personal portfolio website designed to showcase professional information, skills, and projects. The website includes multiple well-structured sections with a responsive layout and smooth user experience across all devices.",
     tech: ["HTML", "CSS", "JavaScript", "Bootstrap"],
-    link:"https://jessiicaa.netlify.app/",
+    link:"https://portfolio-template-sooty-psi.vercel.app/",
   },
    {
     title: "To-Do List Application",
     description:
       " A task management web application that allows users to create, edit, and delete tasks with detailed descriptions. The app supports marking tasks as completed and organizing completed and incomplete tasks into separate categories for easy progress tracking.",
     tech: ["React ", "Redux ", "TailwindCss ", "JavaScript"],
-    link:"https://todo-list-heba.netlify.app/",
+    link:"https://to-do-list-two-jet-69.vercel.app/",
   },
 ];
